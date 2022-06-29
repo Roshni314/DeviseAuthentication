165be1f0c69bb7d41f93d5d65e59bbab91492af5
Roshni314 <roshnipvarsani@gmail.com>
2022-06-29 13:54:03 +0530

    Change file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
 
     def index
         @search = User.ransack(params[:q])
-        @users = @search.result
-        @search.build_condition
-        @users = User.page(params[:page]).per(3)
+        @users = @search.result.page(params[:page]).per(3)
     end
 end
